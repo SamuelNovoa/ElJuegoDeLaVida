ac4a630f19e91e1d7dcdb22e216093ee96b16d9f
Samuel Novoa <samuel.novoa97@gmail.com>
2022-04-27 14:24:17 +0200

    Cambios

---
+++ src/config/Config.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+/**
+ *
+ * @author a21samuelnc
+ */
+public class Config {
+    public final static int TP_WIDTH = 120;
+    public final static int TP_HEIGTH = 80;
+    public final static int DEFAULT_DIFF = 500;
+    
+    public final static int CELL_WIDTH = 10;
+    public final static int BTNS_HEIGTH = 50;
+}
