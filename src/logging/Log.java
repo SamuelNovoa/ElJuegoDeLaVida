835035ee40e37abf26e7afff3254fc611d12b09d
Iago <iagoteis22@gmail.com>
2022-05-05 10:05:46 +0200

    logger

--- src/logging/Log.java
+++ src/logging/Log.java
@@ -3,18 +3,47 @@ package logging;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import models.Profile;
 
 /**
  *
  * @author a21iagoof
  */
 public class Log {
-    public static void write(String msg) {
+    /**
+     * Escritura en arquivo marcando como error
+     * @param msg co string do error
+     */
+    public static void writeErr(String msg) {
+        write("log.txt", ("Error: " + msg));
+    }
+    
+    /**
+     * Escritura en arquivo marcando como advertencia
+     * @param msg co string da advertencia
+     */
+    public static void writeWarning(String msg) {
+        write("log.txt", ("Warning: " + msg));
+    }
+    
+    /**
+     * Rexistrar os datos obtidos polo usuario no xogo
+     * @param user Obxeto co perfil do usuario
+     */
+    public static void writePlayer(Profile user) {
+        write("logUsers.txt", ("User: " + user.name + ", Max. generación: " + user.maxGen));
+    }
+    
+    /**
+     * Escribe no ficheiro, <strong>engadindo ao final deste</strong> os novos datos
+     * @param txt string co nome do arquivo de escritura
+     * @param msg string co mensaxe a escribir no arquivo
+     */
+    private static void write(String txt, String msg) {
         FileWriter fichero = null;
         try {
-            fichero = new FileWriter("logUsers.txt", true);
-            fichero.write(msg + " -- DateTTime: " + LocalDateTime.from(LocalDateTime.now()));
-            
+            fichero = new FileWriter(txt, true);
+            fichero.write(msg + "-" + LocalDateTime.from(LocalDateTime.now()) + "\n");
         } catch (Exception e) {
         } finally {
             try {
@@ -23,4 +52,5 @@ public class Log {
             }
         }
     }
+    
 }
