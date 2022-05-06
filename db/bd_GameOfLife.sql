2c5e70d0f24dea60daed343127cc88821591351d
Iago <iagoteis22@gmail.com>
2022-05-06 10:54:40 +0200

    database

---
+++ db/bd_GameOfLife.sql
@@ -0,0 +1,122 @@
+drop database if exists bd_GameOfLife;
+create database if not exists bd_GameOfLife;
+use bd_GameOfLife;
+
+-- ------------------------------------------------------------ DATA BASE
+drop table if exists `profiles`;
+create table if not exists `profiles`(
+	`id` int unsigned not null auto_increment,
+    `name` varchar(30) not null,
+    
+    primary key (`id`)
+) engine innodb;
+
+
+drop table if exists figures;
+create table if not exists figures(
+	`PROFILE` int unsigned not null,
+    `name` varchar(40) not null,
+	`id` int unsigned not null,
+    `data` blob not null,
+    
+    primary key (`PROFILE`, `id`),
+    
+    foreign key (`PROFILE`) references `profiles` (`id`)
+							on delete cascade
+							on update cascade,
+	index fk_profile (`PROFILE`)
+    
+) engine innodb;
+
+
+drop table if exists movements;
+create table if not exists movements(
+   actions varchar(255)
+) engine innodb;
+
+
+drop view if exists view_data;
+create view view_data
+as
+	select 	p.`id` as ID, 
+			p.`name` as `PROFILE`, 
+            count(f.`id`) as No_figures
+	from `profiles` as p left join figures as f
+						on p.`id` = f.`PROFILE`
+	group by p.`id`;
+
+
+drop view if exists figures_profile;
+create view figures_profile as
+	select	`PROFILE` as `PROFILE`,
+			`name` as Name_figure,
+			`id` as Figure_pos,
+			`data` as Figure
+		from figures;
+
+
+-- ------------------------------------------------------------ DEFAULT DATA
+insert into `profiles` (`name`)
+	values ('Incio sin registro');
+
+
+-- ------------------------------------------------------------  TRIGGERS
+delimiter $$
+
+drop trigger if exists insert_profile $$
+create trigger insert_profile after insert
+	on `profiles`
+    for each row
+    begin
+		insert into movements (actions)
+			values (concat('ADD USER: ', new.`id`, '-', new.`name`));
+    end $$
+
+drop trigger if exists delete_profile $$
+create trigger delete_profile after delete
+	on `profiles`
+    for each row
+    begin
+		insert into movements (actions)
+			values (concat('RMV USER: ', old.`id`, '-', old.`name`));
+    end $$
+
+drop trigger if exists update_profile $$
+create trigger update_profile after update
+	on `profiles`
+    for each row
+    begin
+		insert into movements (actions)
+			values (concat('UPD USER: ', old.`name`, '-', new.`name`));
+    end $$
+
+
+
+drop trigger if exists insert_figure $$
+create trigger insert_figure before insert
+	on figures
+    for each row
+    begin
+		declare counter integer;
+    
+		select No_figures into counter
+			from view_data
+            where ID = new.`PROFILE`;
+            
+		set new.`id` = counter + 1;
+        
+    end $$
+
+drop trigger if exists delete_figure $$
+create trigger delete_figure after delete
+	on figures
+    for each row
+    begin
+		insert into movements (actions)
+			values (concat('RMV FIGURE: ', old.`PROFILE`, '-', old.`id`));
+    end $$
+
+
+
+
+
